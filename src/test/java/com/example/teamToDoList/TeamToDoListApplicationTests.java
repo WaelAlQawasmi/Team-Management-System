f5f44f5c5ceff0a744791762d7ca3c776c05c771
waelalq <wael.abuomr@gmail.com>
2022-04-08 21:24:16 +0300

    create project

---
+++ src/test/java/com/example/teamToDoList/TeamToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
