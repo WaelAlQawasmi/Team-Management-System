aa4edf6b6c8a2dc264e02ac7a22ba76157c4bd96
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-04-11 11:54:16 +0300

    Initial Stage

---
+++ src/main/java/com/example/teamToDoList/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.teamToDoList.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping ("/")
+    public String homePage () {
+        return "login";
+    }
+}
