8f562aa917534054347d868ee8535e7b8236f9c8
waelalq <wael.abuomr@gmail.com>
2022-04-12 14:03:27 +0300

    solved

---
+++ src/main/java/com/example/teamToDoList/models/todolist.java
@@ -0,0 +1,26 @@
+package com.example.teamToDoList.models;
+
+import lombok.*;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Setter
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class todolist  {
+
+    @Setter(value = AccessLevel.NONE)
+    @Id
+    @GeneratedValue
+    private long id;
+    @NonNull
+    private String name;
+
+
+
+}
