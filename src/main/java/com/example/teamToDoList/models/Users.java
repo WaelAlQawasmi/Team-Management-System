ae3d6d08dda80f23471614735f67a2cf1b873477
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-04-12 22:33:22 +0300

    login and signup Authe

--- src/main/java/com/example/teamToDoList/models/Users.java
+++ src/main/java/com/example/teamToDoList/models/Users.java
@@ -7,42 +7,133 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 
-import javax.persistence.Id;
 import java.util.Collection;
 
 
-@Setter
-@Getter
-@NoArgsConstructor
-@RequiredArgsConstructor
+
 @Entity
 
-
 public class Users implements UserDetails {
-    @Setter(value = AccessLevel.NONE)
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     @NonNull
-    @Column(unique = true)
-    private String username;
+    private String firstName;
     @NonNull
-    private String password;
-    @NonNull
-    private String FirstName;
-    @NonNull
-    private String LastName;
+    private String lastName;
     @NonNull
     private String email;
     @NonNull
     private String birthDay;
     @NonNull
+    private String position;
+    @NonNull
+    private String address;
+    @NonNull
     private String phone;
+    private String username;
+    @NonNull
+    private String password;
 
+    public Users() {}
+
+    public Users(@NonNull String firstName, @NonNull String lastName, @NonNull String email, @NonNull String birthDay, @NonNull String position, @NonNull String address, @NonNull String phone, String username, @NonNull String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDay = birthDay;
+        this.position = position;
+        this.address = address;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
