309387531ee95ebb3e38130eb6b4d1e6069fa7c9
motasemZaidAlkilani <m.kilany1999@gmail.com>
2022-04-13 18:59:12 +0300

    cards in myTask page ,show cards and username inside it , and able to go to tolist profile.

--- src/main/java/com/example/teamToDoList/Repositories/ToDoListRepositories.java
+++ src/main/java/com/example/teamToDoList/Repositories/ToDoListRepositories.java
@@ -1,6 +1,7 @@
 package com.example.teamToDoList.Repositories;
 
 import com.example.teamToDoList.models.ToDoList;
+import com.example.teamToDoList.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
