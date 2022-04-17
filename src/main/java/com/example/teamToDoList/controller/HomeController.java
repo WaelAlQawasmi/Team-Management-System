dbf729df9d5fa69900a669d26d03187d517cb480
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-04-18 02:14:48 +0300

    resolve Conflicts

--- src/main/java/com/example/teamToDoList/controller/HomeController.java
+++ src/main/java/com/example/teamToDoList/controller/HomeController.java
@@ -230,36 +230,34 @@ public String myTaskPage(Model model){
 
     @PostMapping ("/listprofile/addtask/{listId}") // add task  on to do list
     public RedirectView addtask (Principal p, Model model,@PathVariable Long listId, @RequestParam String username , @RequestParam String task, @RequestParam String description) {
-        Users member=usersRepositorie.findByusername(username);
+        Users member = usersRepositorie.findByusername(username);
 
-        ToDoList toDoList=ToDoListRepositories.findById(listId)
+        ToDoList toDoList = ToDoListRepositories.findById(listId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + listId));
         Users admin = usersRepositorie.findByusername(p.getName());
 
-        if(toDoList.getMembers().contains(member)||toDoList.getUsers().equals(admin)) {
-            ToDoListItems newItem= new ToDoListItems(task,"0", description);
+        if (toDoList.getMembers().contains(member) || toDoList.getUsers().equals(admin)) {
+            ToDoListItems newItem = new ToDoListItems(task, "0", description);
 
-         System.out.println(toDoList.getUsers().getId()+".....................99");
-        System.out.println(admin.getId()+".....................66");
-        if(toDoList.getUsers().getId()==admin.getId()){
-        if(toDoList.getMembers().contains(member)||toDoList.getUsers().getId()==member.getId()) {
-            ToDoListItems newItem= new ToDoListItems(task,"0");
-
-          
-            newItem.setTodolist(toDoList);
-            newItem.setUsersmember(member);
-            ToDoListItemsRepositories.save(newItem);
-            return new RedirectView("/listprofile/"+listId+"?adddone=on2e")  ;
+            System.out.println(toDoList.getUsers().getId() + ".....................99");
+            System.out.println(admin.getId() + ".....................66");
+            if (toDoList.getUsers().getId() == admin.getId()) {
+                if (toDoList.getMembers().contains(member) || toDoList.getUsers().getId() == member.getId()) {
+                    ToDoListItems newItem1 = new ToDoListItems(task, "0", description);
 
 
+                    newItem1.setTodolist(toDoList);
+                    newItem1.setUsersmember(member);
+                    ToDoListItemsRepositories.save(newItem1);
+                    return new RedirectView("/listprofile/" + listId + "?adddone=on2e");
 
 
+                }
+                return new RedirectView("/listprofile/" + listId + "?errorone=1");
+            }
         }
-            return new RedirectView("/listprofile/"+listId+"?errorone=1")  ;
-        }
-
-        return new RedirectView("/listprofile/"+listId+"?eradmin=1")  ;
-       }
+        return new RedirectView("/listprofile/" + listId + "?eradmin=1");
+    }
 
 
     @GetMapping ("/taskslist")
